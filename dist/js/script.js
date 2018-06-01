559f2e09ca8d7ac3639cebc58a51db6058a79102
Timafei <33371343+TimaG13@users.noreply.github.com>
2018-06-01 17:40:56 +0300

    newy

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,15 +4,15 @@ $( document ).ready(function() {
 
     $(".main .side_menu .opened .button").click(function(){
         menu_state = 1-menu_state;
-        $(".main .side_menu .button.tog .svg-1").removeClass("menu");
-        $(".main .side_menu .button.tog .svg-1").removeClass("cross");
+        $(".main .side_menu .button.tog").removeClass("menu");
+        $(".main .side_menu .button.tog").removeClass("cross");
         //$(".main .side_menu .closed").addClass("active");
         if(menu_state==1){
-            $(".main .side_menu .button.tog .svg-1").addClass("cross");
+            $(".main .side_menu .button.tog").addClass("cross");
             $(".main .side_menu .closed").addClass("active");
         }
         if(menu_state==0){
-            $(".main .side_menu .button.tog .svg-1").addClass("menu");
+            $(".main .side_menu .button.tog").addClass("menu");
             $(".main .side_menu .closed").removeClass("active");
         }
     });
@@ -21,4 +21,16 @@ $( document ).ready(function() {
     //	$(".main .side_menu .closed").removeClass("active");
     //});
 
+    $( ".button.tog.menu" ).click(function() {
+        $( this ).toggleClass( "d_block" );
+    });
+
+
+    // slide
+    $('.slid_master').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true
+    });
 });
