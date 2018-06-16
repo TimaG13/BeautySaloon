322abf02e2fa3f95554e2705c92f7f29602ee7f7
Timafei <33371343+TimaG13@users.noreply.github.com>
2018-06-16 15:00:39 +0300

    asdasdas

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,46 +1,44 @@
 var menu_state = 0;
-$( document ).ready(function() {
+$(document).ready(function () {
     // $(".opened").css("height",$(".closed").css("height"));
 
-    $(".main .side_menu .opened .button").click(function(){
-        menu_state = 1-menu_state;
+    $(".main .side_menu .opened .button").click(function () {
+        menu_state = 1 - menu_state;
         $(".main .side_menu .button.tog").removeClass("menu");
-        $(".main .side_menu .button.tog").removeClass("cross");
+        $(".main .side_menu .button.tog").removeClass("cross d_block");
         //$(".main .side_menu .closed").addClass("active");
-        if(menu_state==1){
-            $(".main .side_menu .button.tog").addClass("cross");
+        if (menu_state == 1) {
+            $(".main .side_menu .button.tog").addClass("cross d_block");
             $(".main .side_menu .closed").addClass("active");
         }
-        if(menu_state==0){
+        if (menu_state == 0) {
             $(".main .side_menu .button.tog").addClass("menu");
             $(".main .side_menu .closed").removeClass("active");
         }
+        // if (menu_state == 0) {
+        //     $(".main .side_menu .opened .button.tog.cross").removeClass("cross");
+        //     $(".main .side_menu .opened .button.tog.cross").removeClass("menu");
+        // }
     });
 
-    //$(".main .side_menu .closed > .button").click(function(){
-    //	$(".main .side_menu .closed").removeClass("active");
-    //});
-
-    $( ".button.tog.menu" ).click(function() {
-        $( this ).toggleClass( "d_block" );
-    });
 
     // menu click document
-    jQuery(function($){
-        $(document).mouseup(function (e){
+    jQuery(function ($) {
+        $(document).mouseup(function (e) {
             var div = $(".closed");
             if (!div.is(e.target)
-                && div.has(e.target).length === 0) {
+                 && div.has(e.target).length === 0) {
                 div.removeClass('active');
-                $( "a.button.tog" ).removeClass("d_block");
+                $("a.button.tog").removeClass("cross d_block");
+                $("a.button.tog").addClass("menu");
             }
         });
     });
 
     // slide
     $(".owl-carousel").owlCarousel({
-        margin:10,
-        items:3,
-        loop:true
+        margin: 10,
+        items: 3,
+        loop: true
     });
 });
